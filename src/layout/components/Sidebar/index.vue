66d335ba25f9730f956fd1c76787fc2d186b54ef
zweiqin <zweiqin88@163.com>
2022-11-07 14:50:18 +0800

    2022.11.07 - 补充提交

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,22 +1,22 @@
 <template>
-  <div :class="{'has-logo':showLogo}">
-    <logo v-if="showLogo" :collapse="isCollapse" />
-    <el-scrollbar wrap-class="scrollbar-wrapper">
-      <el-menu
-        :default-active="activeMenu"
-        :collapse="isCollapse"
-        :background-color="variables.menuBg"
-        :text-color="variables.menuText"
-        :unique-opened="true"
-        :active-text-color="variables.menuActiveText"
-        :collapse-transition="false"
-        mode="vertical"
-      >
-        <sidebar-item v-for="route in menuList" :key="route.route" :item="route" :base-path="route.route" />
-        <!--        <sidebar-item v-for="route in permission_routes" :key="route.path" :item="route" :base-path="route.path" />-->
-      </el-menu>
-    </el-scrollbar>
-  </div>
+	<div :class="{'has-logo':showLogo}">
+		<logo v-if="showLogo" :collapse="isCollapse" />
+		<el-scrollbar wrap-class="scrollbar-wrapper">
+			<el-menu
+				:default-active="activeMenu"
+				:collapse="isCollapse"
+				:background-color="variables.menuBg"
+				:text-color="variables.menuText"
+				:unique-opened="true"
+				:active-text-color="variables.menuActiveText"
+				:collapse-transition="false"
+				mode="vertical"
+			>
+				<sidebar-item v-for="route in menuList" :key="route.route" :item="route" :base-path="route.route" />
+				<!--        <sidebar-item v-for="route in permission_routes" :key="route.path" :item="route" :base-path="route.path" />-->
+			</el-menu>
+		</el-scrollbar>
+	</div>
 </template>
 
 <script>
@@ -27,35 +27,35 @@ import variables from '@/styles/variables.scss'
 import localstorage from '@/utils/localStorage'
 
 export default {
-  components: { SidebarItem, Logo },
-  computed: {
-    ...mapGetters([
-      'permission_routes',
-      'sidebar'
-    ]),
-    activeMenu() {
-      const route = this.$route
-      const { meta, path } = route
-      // if set path, the sidebar will highlight the path you set
-      if (meta.activeMenu) {
-        return meta.activeMenu
-      }
-      return path
-    },
-    showLogo() {
-      return this.$store.state.settings.sidebarLogo
-    },
-    variables() {
-      return variables
-    },
-    isCollapse() {
-      return !this.sidebar.opened
-    }
-  },
-  data() {
-    return {
-      menuList: localstorage.get('admin_menu_list')
-    }
-  }
+	components: { SidebarItem, Logo },
+	computed: {
+		...mapGetters([
+			'permission_routes',
+			'sidebar'
+		]),
+		activeMenu() {
+			const route = this.$route
+			const { meta, path } = route
+			// if set path, the sidebar will highlight the path you set
+			if (meta.activeMenu) {
+				return meta.activeMenu
+			}
+			return path
+		},
+		showLogo() {
+			return this.$store.state.settings.sidebarLogo
+		},
+		variables() {
+			return variables
+		},
+		isCollapse() {
+			return !this.sidebar.opened
+		}
+	},
+	data() {
+		return {
+			menuList: localstorage.get('admin_menu_list')
+		}
+	}
 }
 </script>
